e5737dedb99f78246115c9a8eb6a4611d8d02f13
Mischa Taylor <57647141+taylorific@users.noreply.github.com>
2022-10-16 12:35:00 -0400

    Import sles repo (#1)
    * Import sles repo

--- __main__.py
+++ __main__.py
@@ -1,10 +1,30 @@
-"""A Python Pulumi program"""
+"""Manage boxcutter org"""
 
 from boxcutter.scm.github import GitHubRepository, GitHubRepositoryArgs
 
+GitHubRepository(
+    "sles",
+    GitHubRepositoryArgs(
+        description="SUSE Linux Enterprise Server templates written in legacy JSON",
+    ),
+)
 GitHubRepository(
     "windows-ps",
     GitHubRepositoryArgs(
         description="Experimental Windows templates written in legacy JSON based on PowerShell scripts",
     ),
 )
+
+# GitHubRepository(
+#     "sles",
+#     GitHubRepositoryArgs(
+#         description = "SUSE Linux Enterprise Server templates written in legacy JSON",
+#         repository_import="sles",
+#         allow_merge_commit = True,
+#         allow_rebase_merge = True,
+#         archive_on_destroy = None,
+#         auto_init = False,
+#         delete_branch_on_merge = False,
+#         license_template = None,
+#     ),
+# )
